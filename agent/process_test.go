8d3803a66cbd47886ca2e41ba00f1b35130e0a06
Diullei <diullei@gmail.com>
2018-01-25 23:01:21 +0100

    test two commands

--- agent/process_test.go
+++ agent/process_test.go
@@ -8,13 +8,46 @@ import (
 
 var sep = string(os.PathSeparator)
 
-func Test_Process_Run(t *testing.T) {
+func Test_Process_Run_one_cmd(t *testing.T) {
 	proc := Process{}
+
 	expected := "test01\n"
 	actual := "-error-"
 
 	ok := false
 	proc.Run("python", []string{fmt.Sprintf(".%stest_assets%stest01.py", sep, sep)}, func(resul []byte) {
+		if string(resul) == expected {
+			ok = true
+		}
+	})
+
+	if !ok {
+		t.Errorf("Expecting '%s', got '%s'", expected, actual)
+	}
+}
+
+func Test_Process_Run_two_cmd(t *testing.T) {
+	proc := Process{}
+
+	expected := "test01\n"
+	actual := "-error-"
+
+	ok := false
+	proc.Run("python", []string{fmt.Sprintf(".%stest_assets%stest01.py", sep, sep)}, func(resul []byte) {
+		if string(resul) == expected {
+			ok = true
+		}
+	})
+
+	if !ok {
+		t.Errorf("Expecting '%s', got '%s'", expected, actual)
+	}
+
+	expected = "test02\n"
+	actual = "-error-"
+
+	ok = false
+	proc.Run("python", []string{fmt.Sprintf(".%stest_assets%stest02.py", sep, sep)}, func(resul []byte) {
 		if string(resul) == expected {
 			ok = true
 		}
